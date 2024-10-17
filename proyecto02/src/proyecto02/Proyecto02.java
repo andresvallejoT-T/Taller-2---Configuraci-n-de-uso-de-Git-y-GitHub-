be73ee645dd88fcec614abd4a2ea30b9f23879b2
andresvallejoT-T <andresvallejo102006@gmail.com>
2024-10-17 13:58:59 -0500

    tercer commit, trabajando con Git y GitHubClassroom

---
+++ proyecto02/src/proyecto02/Proyecto02.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto02;
+
+/**
+ *
+ * @author Andrés
+ */
+public class Proyecto02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Ejemplo dos básico de Java Versionado a través de Git Manejado desde GitHub Además se usar GitHubClassroom");
+    }
+    
+}
