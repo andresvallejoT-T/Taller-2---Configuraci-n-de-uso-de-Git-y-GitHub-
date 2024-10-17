3819d49482d8e9620f5c72ff10d15764d73cb555
andresvallejoT-T <andresvallejo102006@gmail.com>
2024-10-17 13:48:53 -0500

    primer commit

---
+++ proyecto01/src/proyecto01/Proyecto01.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto01;
+
+/**
+ *
+ * @author Andrés
+ */
+public class Proyecto01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Ejemplo básico de Java Versionado a través de Git Manejado desde GitHub");
+    }
+    
+}
